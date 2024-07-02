7a17a553327289f4cf1928264d6fc050ab1b23b7
Ericp95 <ericp6960@gmail.com>
2024-07-02 10:55:29 -0300

    creando record en la carpeta dto  datos resumidos

---
+++ foroAlura/src/main/java/com/alura/foroAlura/dto/etiquetas/cursos/DatosResumidosCurso.java
@@ -0,0 +1,10 @@
+package com.alura.foroAlura.dto.etiquetas.cursos;
+
+import foro.modelo.Etiqueta;
+
+public record DatosResumidosCurso (Long id, String nombre) {
+
+	public DatosResumidosCurso(Etiqueta curso) {
+		this(curso.getId(), curso.getNombre());
+	}
+}
