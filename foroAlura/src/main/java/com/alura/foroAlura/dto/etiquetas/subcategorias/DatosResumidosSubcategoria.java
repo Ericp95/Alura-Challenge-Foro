007cd9d9454ab5b96104a540b7744382d2eb1a02
Ericp95 <ericp6960@gmail.com>
2024-07-02 10:57:04 -0300

    creando record en la carpeta dto  datos resumidos por subcategoria

---
+++ foroAlura/src/main/java/com/alura/foroAlura/dto/etiquetas/subcategorias/DatosResumidosSubcategoria.java
@@ -0,0 +1,16 @@
+package com.alura.foroAlura.dto.etiquetas.subcategorias;
+
+import foro.modelo.Etiqueta;
+
+public record DatosResumidosSubcategoria(
+		Long id,
+		String nombre
+		) {
+
+	public DatosResumidosSubcategoria(Etiqueta subcategoria) {
+		this(
+				subcategoria.getId(),
+				subcategoria.getNombre()
+			);
+	}
+}
