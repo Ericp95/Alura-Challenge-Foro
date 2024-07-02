e280a9c39496fceb60e70ce93aef7a3061b9e1c4
Ericp95 <ericp6960@gmail.com>
2024-07-02 10:53:42 -0300

    creando javaclass  datos resumidos  por categoria

---
+++ foroAlura/src/main/java/com/alura/foroAlura/dto/etiquetas/categorias/DatosResumidosCategoria.java
@@ -0,0 +1,10 @@
+package com.alura.foroAlura.dto.etiquetas.categorias;
+
+import foro.modelo.Etiqueta;
+
+public record DatosResumidosCategoria(Long id, String nombre) {
+
+	public DatosResumidosCategoria(Etiqueta categoria) {
+		this(categoria.getId(), categoria.getNombre());
+	}
+}
