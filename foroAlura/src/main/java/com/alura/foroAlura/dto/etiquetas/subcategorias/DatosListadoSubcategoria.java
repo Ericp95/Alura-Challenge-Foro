f14593c63c2754b6c9ea90be96ddb46f235823c6
Ericp95 <ericp6960@gmail.com>
2024-07-02 10:56:46 -0300

    creando record en la carpeta dto  datos listados por subcategoria

---
+++ foroAlura/src/main/java/com/alura/foroAlura/dto/etiquetas/subcategorias/DatosListadoSubcategoria.java
@@ -0,0 +1,19 @@
+package com.alura.foroAlura.dto.etiquetas.subcategorias;
+
+import foro.dto.etiquetas.categorias.DatosResumidosCategoria;
+import foro.modelo.Etiqueta;
+
+public record DatosListadoSubcategoria(
+		Long id,
+		String nombre,
+		DatosResumidosCategoria categoria
+		) {
+
+	public DatosListadoSubcategoria(Etiqueta subcategoria) {
+		this(
+				subcategoria.getId(),
+				subcategoria.getNombre(),
+				new DatosResumidosCategoria(subcategoria.getEtiquetaPadre())
+			);
+	}
+}
