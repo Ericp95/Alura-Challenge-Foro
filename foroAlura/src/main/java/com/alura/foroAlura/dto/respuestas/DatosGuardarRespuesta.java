a72ceb660533713570ced430c2a2eaddc851e123
Ericp95 <ericp6960@gmail.com>
2024-07-03 08:50:19 -0300

    creando record en la carpeta dto  datos guardados de una respuesta

---
+++ foroAlura/src/main/java/com/alura/foroAlura/dto/respuestas/DatosGuardarRespuesta.java
@@ -0,0 +1,15 @@
+package com.alura.foroAlura.dto.respuestas;
+
+import foro.modelo.Respuesta;
+import jakarta.validation.constraints.NotBlank;
+
+public record DatosGuardarRespuesta(
+		@NotBlank(message = "El mensaje es obligatorio")
+		String mensaje
+		) {
+
+	public DatosGuardarRespuesta(Respuesta respuesta) {
+		this(respuesta.getMensaje());
+	}
+
+}
