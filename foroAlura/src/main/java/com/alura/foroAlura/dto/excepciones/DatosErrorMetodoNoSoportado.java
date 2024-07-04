90331814d55e4aba4b17e274033deed67f613357
Ericp95 <ericp6960@gmail.com>
2024-07-04 09:58:00 -0300

    creando record en la carpeta dto excepciones datos error metodo no soportado

---
+++ foroAlura/src/main/java/com/alura/foroAlura/dto/excepciones/DatosErrorMetodoNoSoportado.java
@@ -0,0 +1,45 @@
+package com.alura.foroAlura.dto.excepciones;
+
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.HttpRequestMethodNotSupportedException;
+
+import java.time.LocalDateTime;
+import java.util.Set;
+
+public record DatosErrorMetodoNoSoportado(
+		int codigoDeError,
+		HttpStatus status,
+		LocalDateTime fechaYHora,
+		String mensaje,
+		String path) {
+
+	public DatosErrorMetodoNoSoportado(
+			HttpRequestMethodNotSupportedException excepcion, 
+			HttpServletRequest request) {
+		this(
+				405,
+				HttpStatus.METHOD_NOT_ALLOWED,
+				LocalDateTime.now(),
+				generarMensajeDeError(excepcion),
+				request.getRequestURI()
+			);
+	}
+
+	private static String generarMensajeDeError(HttpRequestMethodNotSupportedException excepcion) {
+		Set<HttpMethod> conjuntoMetodosPermitidos = excepcion.getSupportedHttpMethods();
+
+		StringBuilder builder = new StringBuilder();
+		builder.append("El método usado: ");
+	    builder.append(excepcion.getMethod());
+	    builder.append(" no está permitido.");
+
+	    if(conjuntoMetodosPermitidos != null && conjuntoMetodosPermitidos.size() > 0) {
+			builder.append(" Los métodos permitidos son:");
+			conjuntoMetodosPermitidos.forEach(metodo -> builder.append(" " + metodo));
+		}
+
+	    return builder.toString();
+	}
+}
