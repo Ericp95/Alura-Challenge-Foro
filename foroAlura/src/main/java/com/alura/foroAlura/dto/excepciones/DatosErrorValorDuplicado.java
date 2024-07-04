c141c218ebc30befdc649d6f885ea7b94a008aac
Ericp95 <ericp6960@gmail.com>
2024-07-04 09:59:05 -0300

    creando record en la carpeta dto excepciones datos error valor duplicado

---
+++ foroAlura/src/main/java/com/alura/foroAlura/dto/excepciones/DatosErrorValorDuplicado.java
@@ -0,0 +1,40 @@
+package com.alura.foroAlura.dto.excepciones;
+
+import foro.excepciones.InfoExcepcionesPersonalizadas;
+import org.hibernate.exception.ConstraintViolationException;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.http.HttpStatus;
+
+import java.time.LocalDateTime;
+import java.util.Map;
+
+public record DatosErrorValorDuplicado(
+		int codigoDeError,
+		HttpStatus status,
+		LocalDateTime fechaYHora,
+		String mensaje) {
+
+	public DatosErrorValorDuplicado(DataIntegrityViolationException excepcion) {
+		this(400, HttpStatus.BAD_REQUEST, LocalDateTime.now(), generarMensajeDeError(excepcion));
+	}
+
+	private static String generarMensajeDeError(DataIntegrityViolationException excepcion) {
+		ConstraintViolationException violaciónDeRestricción = (ConstraintViolationException) excepcion.getCause();
+
+		if (violaciónDeRestricción == null) {
+			return null;			
+		}
+
+		String nombreDeRestricción = violaciónDeRestricción.getConstraintName();
+
+		for (Map.Entry<String, String> excepcionPropia : InfoExcepcionesPersonalizadas.MENSAJES.entrySet()) {
+			String identificador = excepcionPropia.getKey();
+
+			if (nombreDeRestricción.contains(identificador)) {
+				return excepcionPropia.getValue();
+			}
+		}
+
+		return null;
+	}
+}
