e239fcd8640ed5f9521e6074a6de7f1f1053db0c
Ericp95 <ericp6960@gmail.com>
2024-07-08 09:02:20 -0300

    creando  la carpeta controller la javaclass subcategoria controlador

---
+++ foroAlura/src/main/java/com/alura/foroAlura/controller/SubcategoriaControlador.java
@@ -0,0 +1,90 @@
+package com.alura.foroAlura.controller;
+
+import foro.dto.etiquetas.subcategorias.DatosCompletosSubcategoria;
+import foro.dto.etiquetas.subcategorias.DatosGuardarSubcategoria;
+import foro.dto.etiquetas.subcategorias.DatosListadoSubcategoria;
+import foro.servicios.EtiquetaServicio;
+import jakarta.transaction.Transactional;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.net.URI;
+
+@RestController
+@RequestMapping("api/v1/subcategorias")
+public class SubcategoriaControlador {
+
+	@Autowired
+	private EtiquetaServicio etiquetaServicio;
+
+	@GetMapping
+	public ResponseEntity<Page<Record>> listarSubcategorias(
+			@RequestParam(name = "categoria", required = false)
+			Long categoriaId,
+
+			@PageableDefault(size = 25)
+			Pageable paginacion) {
+
+		Page<Record> pagina = null;
+
+		if(categoriaId != null) {
+			pagina = etiquetaServicio.listarSubcategoriasPorCategoriaId(categoriaId, paginacion); 			
+
+		} else {
+			pagina = etiquetaServicio.listarSubcategorias(paginacion);
+		}
+
+		return ResponseEntity.ok(pagina);
+	}
+
+	@GetMapping("/{subcategoriaId}")
+	public ResponseEntity<DatosCompletosSubcategoria> encontrarSubcategoriaPorId(@PathVariable Long subcategoriaId) {
+		DatosCompletosSubcategoria subcategoria = etiquetaServicio.encontrarSubcategoriaPorId(subcategoriaId);
+		return ResponseEntity.ok(subcategoria);
+	}
+
+	@PostMapping
+	@Transactional
+	public ResponseEntity<DatosListadoSubcategoria> crearSubcategoria(
+			@RequestBody
+			@Valid
+			DatosGuardarSubcategoria datosSubcategoria,
+
+			UriComponentsBuilder uriComponentsBuilder) {
+
+		DatosListadoSubcategoria subcategoria = etiquetaServicio.crearSubcategoria(datosSubcategoria);
+
+		URI url = uriComponentsBuilder.path("subcategorias/{subcategoriaId}").buildAndExpand(subcategoria.id()).toUri();
+
+		return ResponseEntity.created(url).body(subcategoria);
+	}
+
+	@PutMapping("/{subcategoriaId}")
+	@Transactional
+	public ResponseEntity<DatosListadoSubcategoria> editarSubcategoria(
+			@PathVariable 
+			Long subcategoriaId,
+
+			@RequestBody
+			@Valid
+			DatosGuardarSubcategoria datosSubcategoria) {
+
+		DatosListadoSubcategoria subcategoria = etiquetaServicio.editarSubcategoria(subcategoriaId, datosSubcategoria);
+
+		return ResponseEntity.ok(subcategoria);
+	}
+
+	@DeleteMapping("/{subcategoriaId}")
+	@Transactional
+	public ResponseEntity<Void> eliminarSubcategoria(@PathVariable Long subcategoriaId) {
+		etiquetaServicio.eliminarSubcategoria(subcategoriaId);
+		return ResponseEntity.noContent().build();
+	}
+
+}
