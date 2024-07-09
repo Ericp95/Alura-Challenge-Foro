5dbeb5a5a4659fec760e140e5235ed5563482f06
Ericp95 <ericp6960@gmail.com>
2024-07-09 08:52:25 -0300

    creando  la carpeta excepciones creando dentro pertenencial invalida excepciones

---
+++ foroAlura/src/main/java/com/alura/foroAlura/excepciones/PertenenciaInvalidaExcepcion.java
@@ -0,0 +1,10 @@
+package com.alura.foroAlura.excepciones;
+
+public class PertenenciaInvalidaExcepcion extends RuntimeException {
+
+	private static final long serialVersionUID = 1L;
+
+	public PertenenciaInvalidaExcepcion(String mensaje) {
+		super(mensaje);
+	}
+}
