729bd1b7291c82a1a5507ad95966ad6483703622
Ericp95 <ericp6960@gmail.com>
2024-07-10 10:19:39 -0300

    creando  la carpeta seguridad dentro de ella creamos java class seguridad utilidades

---
+++ foroAlura/src/main/java/com/alura/foroAlura/seguridad/SeguridadUtilidades.java
@@ -0,0 +1,45 @@
+package com.alura.foroAlura.seguridad;
+
+import foro.modelo.Publicacion;
+import foro.modelo.Respuesta;
+import foro.modelo.Usuario;
+import foro.repositorio.PublicacionRepositorio;
+import foro.repositorio.RespuestaRepositorio;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+
+@Component
+public class SeguridadUtilidades {
+
+	@Autowired
+	PublicacionRepositorio publicacionRepositorio;
+
+	@Autowired
+	RespuestaRepositorio respuestaRepositorio;
+
+	public static Usuario getUsuarioAutenticado() {
+		var datosAutenticacion = SecurityContextHolder.getContext().getAuthentication();
+		UsuarioSeguridad usuarioSeguridad = (UsuarioSeguridad) datosAutenticacion.getPrincipal();
+		return usuarioSeguridad.getUsuario();
+	}
+
+	public boolean esAutor(Long recursoId, String clase) {
+		String correoAutorAutenticado = SecurityContextHolder.getContext().getAuthentication().getName();
+
+		if(Publicacion.class.getSimpleName().equals(clase)) {
+
+			Publicacion publicacion = publicacionRepositorio.getReferenceById(recursoId);
+			String correoAutorPublicacion = publicacion.getAutor().getCorreo(); 
+			return correoAutorAutenticado.equals(correoAutorPublicacion);
+
+		} else if (Respuesta.class.getSimpleName().equals(clase)) {
+
+			Respuesta respuesta = respuestaRepositorio.getReferenceById(recursoId);
+			 String correoAutorRespuesta = respuesta.getAutor().getCorreo(); 
+
+			return correoAutorAutenticado.equals(correoAutorRespuesta);
+		}
+		return false;
+	}
+}
